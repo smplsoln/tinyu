207fb71d4f82b2520f6c45197176c5925e49dedc
Prashant R <smplsoln@gmail.com>
2021-09-29 04:48:07 -0600

    Add new urls POST handling in server js

--- server.js
+++ server.js
@@ -3,7 +3,9 @@ const express = require('express');
 // const path = require('path');
 const favicon = require('serve-favicon');
 const morgan = require('morgan');
+
 // CONSTANTS
+const RANDOM_STR_LENGTH = 6;
 const SERVER_PORT = 3000;
 const HTTP_STATUS = {
   GET_OK: 200,
@@ -13,6 +15,7 @@ const APP_URLS = {
   home: '/',
   urls: '/urls',
   shortUrl: '/urls/:shortURL',
+  urlsNew: "/urls/new",
   favicon: '/favicon.ico',
 
   catchAll: '*'
@@ -28,6 +31,18 @@ const urlDbObj = {
   "9sm5xK": "http://www.google.com"
 };
 
+// Generate random string
+const generateRandomString = (strLen) => {
+  let rndmStr = '';
+  let charSet = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let charSetLen = charSet.length;
+  for (let i = 0; i < strLen; i++) {
+    rndmStr += charSet.charAt(Math.floor(Math.random() *
+      charSetLen));
+  }
+  return rndmStr;
+};
+
 // Init Express app
 const app = express();
 
@@ -36,7 +51,7 @@ app.set('view engine', 'ejs');
 
 // middleware inits
 app.use(favicon(RESOURCES.favicon));
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(morgan('dev'));
 
 // resource-handler mappings
@@ -47,17 +62,29 @@ app.get(APP_URLS.home, (req, res) => {
 app.get(APP_URLS.urls, (req, res) => {
   // res.status(HTTP_STATUS.GET_OK).send(`TinyU URLs: "${JSON.stringify(urlDbObj)}"`);
   // res.status(HTTP_STATUS.GET_OK).json(urlDbObj);
-  const templateVars = { urls: urlDbObj };
-  res.render("urls_index", templateVars);
+  const templatelets = { urls: urlDbObj };
+  res.render("urls_index", templatelets);
 });
 
+app.get(APP_URLS.urlsNew, (req, res) => {
+  res.render("urls_new");
+});
+
+app.post(APP_URLS.urls, (req, res) => {
+  let rndmStrId = generateRandomString(RANDOM_STR_LENGTH);
+  urlDbObj[rndmStrId] = req.body.longURL;
+  res.send("ok");
+});
+
+// /url/:id here
 app.get(APP_URLS.shortUrl, (req, res) => {
-  const templateVars = {
+  const templatelets = {
     shortURL: req.params.shortURL,
     longURL: `${urlDbObj[req.params.shortURL]}`
   };
-  res.render("urls_show", templateVars);
+  res.render("urls_show", templatelets);
 });
+
 
 
 // catch-all for unknown resource links
